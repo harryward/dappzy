f7d8854d46252a65f4d9e00a82189dd9087d9476
Harry Ward <harry@kayle.me>
2018-02-25 16:28:32 -0700

    block 12

--- dappzy.js
+++ dappzy.js
@@ -1,6 +1,7 @@
 Web3 = require('web3');
-web3 = new Web3(new Web3.providers.HttpProvider('http://72.174.42.154:8545')) // RINKEBY
+
 //  web3 = new Web3(new Web3.providers.HttpProvider('http://72.174.42.154:2241')) // PRIVATE
+web3 = new Web3(new Web3.providers.HttpProvider('http://72.174.42.154:8547')) // PRIVATE TWO
 console.log('dappzy connecting....')
 
 if (web3) {
