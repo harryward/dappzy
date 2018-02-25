4dc268d12595b6b42de9aa179517a09779a6e201
Harry Ward <harry@kayle.me>
2018-02-25 13:53:37 -0700

    block 10

--- dappzy.js
+++ dappzy.js
@@ -1,11 +1,12 @@
 Web3 = require('web3');
-web3 = new Web3(new Web3.providers.HttpProvider('http://72.174.42.154:8545')) // RINKEBY TESTNET
+// web3 = new Web3(new Web3.providers.HttpProvider('http://72.174.42.154:8545')) // RINKEBY
+ web3 = new Web3(new Web3.providers.HttpProvider('http://72.174.42.154:2241')) // PRIVATE
 console.log('dappzy connecting....')
 
-if(web3){
-console.log('dappzy successfully connected....')
-console.log('coinbase',web3.eth.coinbase)
-}else{
+if (web3) {
+    console.log('dappzy successfully connected....')
+    console.log('coinbase', web3.eth.coinbase)
+} else {
     console.log('dappzy - errors during connection....')
 }
 
