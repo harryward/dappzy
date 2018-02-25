11bd3a1c3963f3635219079de0f5ff9c416613f5
Harry Ward <harry@kayle.me>
2018-02-25 13:39:25 -0700

    block 9

--- dappzy.js
+++ dappzy.js
@@ -1,17 +1,12 @@
-// var Promise = require('promise/domains');
-// var unirest = require("unirest");
-// var querystring = require("querystring");
-// var config = require('./config');
-
-
 Web3 = require('web3');
-
-
-// web3 = new Web3(new Web3.providers.HttpProvider('http://72.174.42.154:4099')) // MAIN
-// web3 = new Web3(new Web3.providers.HttpProvider('http://72.174.42.154:2241')) // PRIVATE
 web3 = new Web3(new Web3.providers.HttpProvider('http://72.174.42.154:8545')) // RINKEBY TESTNET
+console.log('dappzy connecting....')
 
-
+if(web3){
+console.log('dappzy successfully connected....')
 console.log('coinbase',web3.eth.coinbase)
+}else{
+    console.log('dappzy - errors during connection....')
+}
 
 
